b926a826c4d05e0dc578f4994963a943b3e1e3e4
Shivam Rajput <rajputshivam8550855@gmail.com>
2026-06-23 07:06:03 +0530

    change

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -256,21 +256,19 @@ function Dashboard() {
   const [isSuccess, setIsSuccess] = useState(false);
   const navigate = useNavigate();
 
-  
+  const fetchBlogs = async () => {
+  try {
+    const res = await getBlogs();
+    setBlogs(res.data);
+  } catch {
+    navigate('/');
+  }
+};
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+// eslint-disable-next-line react-hooks/exhaustive-deps
 useEffect(() => {
   fetchBlogs();
 }, []);
-
-const fetchBlogs = async () => {
-    try {
-      const res = await getBlogs();
-      setBlogs(res.data);
-    } catch {
-      navigate('/');
-    }
-  };
 
   const handleSave = async () => {
     if (!title || !content) { setMessage('Please fill in both fields'); setIsSuccess(false); return; }
